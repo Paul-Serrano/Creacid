140ef8c4a811a74065040e922eb66dfc36ee08a5
Serrano Paul <paul.serrano08374@gmail.com>
2023-11-30 15:28:52 +0100

    test

---
+++ cgu.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Jura:wght@400;600;700&family=Raleway:ital,wght@0,300;0,400;0,600;0,700;0,800;0,900;1,300;1,400;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,400;1,300;1,400;1,700&display=swap"
+        rel="stylesheet"
+        />
+        <link href="dist/output.css" rel="stylesheet" />
+        <title>Le mois sans IA</title>
+    </head>
+    <body>
+        <main class="bg-grey w-100 h-auto font-primary w-full p-10">
+            <div class="grid">
+                <h1 class="text-cgu-title font-introbold">Conditions générales d’utilisation</h1>
+                <div class="grid">
+                    <h3 class="text-content font-introbold my-5">En vigueur au 01/12/2023</h3>
+                    <p>Les présentes conditions générales d'utilisation (dites « CGU ») ont pour objet l'encadrement juridique des modalités de mise à disposition du site et des services par Le Moi Sans IA et de définir les conditions d’accès et d’utilisation des services par « l'Utilisateur ».</p>
+                    <p>Les présentes CGU sont accessibles sur le site à la rubrique «CGU».</p>
+                </div>
+
+                <div class="grid">
+                    <h3 class="text-content font-introbold my-5">ARTICLE 1 : LES MENTIONS LÉGALES</h3>
+                    <p>L’édition et la direction de la publication du site www.lemoisansia.fr est assurée par Jessie Argand, domicilié 44 avenue François Mitterrand, 77500 Chelles.</p>
+                    <p>Numéro de téléphone est 0749461497</p>
+                    <p>Adresse e-mail argand.jessie@gmail.Com.</p>
+                    <p>L'hébergeur du site www.lemoisansia.fr est la société 02Switch.</p>
+                </div>
+
+                <div class="grid">
+                    <h3 class="text-content font-introbold my-5">ARTICLE 2 : ACCÈS AU SITE</h3>
+                    <p>Le site www.lemoisansia.fr permet à l'Utilisateur un accès gratuit aux services suivants :</p>
+                    <p>Le site internet propose les services suivants : Association de sensibilisation.</p>
+                    <p>Le site est accessible gratuitement en tout lieu à tout Utilisateur ayant un accès à Internet. Tous les frais supportés par l'Utilisateur pour accéder au service (matériel informatique, logiciels, connexion Internet, etc.) sont à sa charge.</p>
+                </div>
+
+                <div class="grid">
+                    <h3 class="text-content font-introbold my-5">ARTICLE 3 : COLLECTE DES DONNÉES</h3>
+                    <p></p>
+                </div>
+
+                <div class="grid">
+                    <h3 class="text-content font-introbold my-5"></h3>
+                    <p>Le site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.</p>
+                    <p>En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit :
+            · via un formulaire de contact.</p>
+                </div>
+
+                <div class="grid">
+                    <h3 class="text-content font-introbold my-5">ARTICLE 4 : PROPRIÉTÉ INTELLECTUELLE</h3>
+                    <p>Les marques, logos, signes ainsi que tous les contenus du site (textes, images, son…) font l'objet d'une protection par le Code de la propriété intellectuelle et plus particulièrement par le droit d'auteur. L'Utilisateur doit solliciter l'autorisation préalable du site pour toute reproduction, publication, copie des différents contenus. Il s'engage à une utilisation des contenus du site dans un cadre strictement privé, toute utilisation à des fins commerciales et publicitaires est strictement interdite.</p>
+                    <p>Toute représentation totale ou partielle de ce site par quelque procédé que ce soit, sans l’autorisation expresse de l’exploitant du site Internet constituerait une contrefaçon sanctionnée par l’article L 335-2 et suivants du Code de la propriété intellectuelle.</p>
+                    <p>Il est rappelé conformément à l’article L122-5 du Code de propriété intellectuelle que l’Utilisateur qui reproduit, copie ou publie le contenu protégé doit citer l’auteur et sa source.</p>
+                </div>
+
+                <div class="grid">
+                    <h3 class="text-content font-introbold my-5">ARTICLE 5 : RESPONSABILITÉ</h3>
+                    <p>
+            Les sources des informations diffusées sur le site www.lemoisansia.fr sont réputées fiables mais le site ne garantit pas qu’il soit exempt de défauts, d’erreurs ou d’omissions. Les informations communiquées sont présentées à titre indicatif et général sans valeur contractuelle. Malgré des mises à jour régulières, le site www.lemoisansia.fr ne peut être tenu responsable de la modification des dispositions administratives et juridiques survenant après la publication. De même, le
+            site ne peut être tenue responsable de l’utilisation et de l’interprétation de l’information contenue dans ce site.</p>
+                    <p>Le site www.lemoisansia.fr ne peut être tenu pour responsable d’éventuels virus qui pourraient infecter l’ordinateur ou tout matériel informatique de l’Internaute, suite à une utilisation, à l’accès, ou au téléchargement provenant de ce site.</p>
+                    <p>La responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.</p>
+                </div>
+
+                <div class="grid">
+                    <h3 class="text-content font-introbold my-5">ARTICLE 6 : LIENS HYPERTEXTES</h3>
+                    <p>Des liens hypertextes peuvent être présents sur le site. L’Utilisateur est informé qu’en cliquant sur ces liens, il sortira du site www.lemoisansia.fr. Ce dernier n’a pas de contrôle sur les pages web sur lesquelles aboutissent ces liens et ne saurait, en aucun cas, être responsable de leur contenu.</p>
+                </div>
+
+                <div class="grid">
+                    <h3 class="text-content font-introbold my-5">ARTICLE 7 : COOKIES</h3>
+                    <p>L’Utilisateur est informé que lors de ses visites sur le site, un cookie peut s’installer automatiquement sur son logiciel de navigation. Les cookies sont de petits fichiers stockés temporairement sur le disque dur de l’ordinateur de l’Utilisateur par votre navigateur et qui sont nécessaires à l’utilisation du site www.lemoisansia.fr. Les cookies ne contiennent pas d’information personnelle et ne peuvent pas être utilisés pour identifier quelqu’un. Un cookie contient un identifiant unique, généré aléatoirement et donc anonyme. Certains cookies expirent à la fin de la visite de l’Utilisateur, d’autres restent. L’information contenue dans les cookies est utilisée pour améliorer le site www.lemoisansia.fr.</p>
+                    <p>En naviguant sur le site, L’Utilisateur les accepte. L’Utilisateur pourra désactiver ces cookies par l’intermédiaire des paramètres figurant au sein de son logiciel de navigation.</p>
+                </div>
+                <div class="grid">
+                    <h3 class="text-content font-introbold my-5">ARTICLE 8 : DROIT APPLICABLE ET JURIDICTION COMPÉTENTE</h3>
+                    <p>La législation française s'applique au présent contrat. En cas d'absence de résolution amiable d'un litige né entre les parties, les tribunaux français seront seuls compétents pour en connaître.</p>
+                    <p>Pour toute question relative à l’application des présentes CGU, vous pouvez joindre l’éditeur aux coordonnées inscrites à l’ARTICLE 1. CGU réalisées sur http://legalplace.fr/</p>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
+
