b11dc196668bcfc4311b2f12782c9b845a101e00
pauldecalf <70703497+pauldecalf@users.noreply.github.com>
2023-12-01 10:44:10 +0100

    W3C update Paul D

--- cgu.php
+++ cgu.php
@@ -14,7 +14,7 @@
     </head>
     <body>
         <main class="bg-grey h-auto font-primary w-full">
-            <nav role="navigation" class="fixed w-full z-50 bg-grey">
+            <nav class="fixed w-full z-50 bg-grey">
                 <?php include_once "./view/nav-cgu.html" ;?>
             </nav>
             <div class="grid p-10 pt-[10rem]">
